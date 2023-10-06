43b74a739ac05a5c52d956c83aaf8cf0134697b9
Niboah <o.niboah@gmail.com>
2023-10-07 00:12:00 +0200

    nearest neighbor function

--- src/KDTree.h
+++ src/KDTree.h
@@ -34,6 +34,12 @@ class KDTree {
         // Función para mostrar el árbil k-d
         void print2DUtil(KDNode* root, int ,int);
 
+        // Función que calcula la distancia euclidiana entre 2 puntos
+        double distance(const vector<double>& point1, const vector<double>& point2);
+
+        // Función para encontrar el punto más cercano recursivamente
+        KDNode* findNearest_rec(KDNode* root, const vector<double>& q,KDNode* best, int depth);
+
     public:
         KDTree();
 
@@ -42,6 +48,9 @@ class KDTree {
 
         // Función para destruir el árbol k-d
         void destroy();
+
+        // Función para encontrar el punto más cercano
+        KDNode* findNearest(KDNode* root, const vector<double>& q);
 
         // Función para mostrar el árbil k-d
         void print();
--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,60 @@
 #include <map>
 using namespace std;
 
+double distance(const vector<double>& point1, const vector<double>& point2){
+    if(point1.size() != point2.size())  throw invalid_argument( "points size not equal" );
+
+    double sum=0;
+
+    for(int i=0; i< point1.size();++i)
+        sum += ((point1[i]-point2[i]) * (point1[i]-point2[i]));
+
+    return sqrt(sum);
+}
+
 int main() {
     KDTree tree=KDTree();
-    // Insertar puntos k-dimensionales en el árbol
+
     int n;
     int m;
     cin>>n>>m;
+
+    //point
+    vector<double> epoint(m);
+    for(int i=0;i<m;i++){
+        cin>>epoint[i];
+    }
+
     for (int i=0;i<n;i++) {
         vector<double> point(m);
-        string name;
-        cin>>name;
         for(int j=0;j<m;j++){
             double aux;
             cin>>aux;
             point[j]=aux;
+            cout<<aux<<" ";
         }
+
         tree.insert(point);
+        cout<<" -> "<<distance(epoint,point)<<endl;
+    }
+
+    //result
+    vector<double> result(m);
+    for(int i=0;i<m;i++){
+        cin>>result[i];
     }
 
     // Realizar operaciones en el árbol...
     tree.print();
 
+    cout<<"------"<<endl;
+    cout<<"Point:";
+    for(double d:epoint)cout<<" "<<d;
+    cout<<endl;
+    cout<<"Result "<<tree.findNearest(tree.getRoot(),epoint)->getName()<<endl;
+    cout<<"Correct result:";
+    for(double d:result) cout<<" "<<d;
+    cout<<endl;
     // Destruir el árbol al finalizar
     tree.destroy();
 
