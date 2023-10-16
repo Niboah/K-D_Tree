a628a3a1337458fd6838e488dcf30579c5bf66b8
Niboah <o.niboah@gmail.com>
2023-10-17 01:51:02 +0200

    test result

---
+++ test/runTest.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import matplotlib
+from sklearn.linear_model import LinearRegression
+matplotlib.use('Qt5Agg')
+
+#tipo="estandard"
+tipo="relaxat"
+#tipo="squarish"
+
+df = pd.DataFrame(columns=["N"])
+
+for i in range(2, 7):
+    file = pd.read_csv(f"{tipo}_k{i}.txt", sep=" ", header=None, names=["N", f"K{i}"])
+    df = df.merge(file, on="N", how="outer")
+
+print(df)
+
+fig, axes = plt.subplots(5, 1, figsize=(10, 25))
+
+for i in range(2, 7):
+    ax = axes[i-2]
+    ax.plot(df["N"], df[f"K{i}"], color=plt.cm.jet(np.random.rand()))
+    ax.set_title(f"Scatter plot de KD-Tree {tipo} para K={i}")
+    ax.set_xlabel("N")
+    ax.set_ylabel("coste")
+
+plt.tight_layout()
+plt.savefig(f'plot_{tipo}.png')
+
+fig, axes = plt.subplots(5, 1, figsize=(10, 25))
+for i in range(2, 7):
+    ax = axes[i-2]
+    n = np.log10(df['N'])
+    cost = np.log10(df[f'K{i}'])
+    regresion = LinearRegression().fit(n.values.reshape(-1, 1), cost)
+
+    print(f'K{i}')
+    print("Intercepto:", regresion.intercept_)
+    print("Coeficiente:", regresion.coef_[0])
+
+    ax.scatter(n, cost)
+    ax.plot(n, regresion.predict(n.values.reshape(-1, 1)), color='red')
+    ax.set_title(f"Log Log plot de KD-Tree {tipo} para K={i}")
+    ax.set_xlabel("log10(n)")
+    ax.set_ylabel("log10(cost)")
+plt.tight_layout()
+fig.savefig(f"loglogplot_{tipo}.png")
---
+++ test_multicore/script.py
@@ -0,0 +1,24 @@
+
+
+
+
+for i in range(2, 7):
+    nombre_archivo_entrada = f'relaxat_k{i}.txt'
+
+
+    # Leer el archivo de entrada y almacenar los datos en una lista
+    datos = []
+    with open(nombre_archivo_entrada, 'r') as archivo_entrada:
+        for linea in archivo_entrada:
+            columna1, columna2 = linea.strip().split()  # Suponiendo que las columnas están separadas por espacios en blanco
+            datos.append((int(columna1), columna2))
+
+    # Ordenar los datos por la primera columna
+    datos_ordenados = sorted(datos, key=lambda x: x[0])
+
+    # Guardar el archivo ordenado
+    with open(nombre_archivo_entrada, 'w') as archivo_salida:
+        for dato in datos_ordenados:
+            archivo_salida.write(f"{dato[0]} {dato[1]}\n")
+
+    print(f"Archivo ordenado y guardado como {nombre_archivo_entrada}")
